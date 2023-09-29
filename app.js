2bb2540456421a9af4b8f816d59591baea7fa8d4
Rafaella Matos <prof.rafaella.matos@gmail.com>
2023-09-29 10:27:24 -0300

    configurações iniciais conexão com o bd funcionando

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+const app = express();
+const PORT = 3000;
+
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '123456',
+  database: 'TweetHubDB',
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+  } else {
+    console.log('Conectado ao banco de dados!');
+  }
+});
+
+app.use(express.json());
+
+// Rotas aqui...
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
